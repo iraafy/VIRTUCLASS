8ea92c7343fd8b436f74aee2f25d4a865f58468f
komarr007 <mariorangga000@gmail.com>
2022-05-16 12:27:59 +0700

    [FIX] remove warning invalid foreach

--- admin/mgmt_guru.php
+++ admin/mgmt_guru.php
@@ -147,27 +147,29 @@
 				</thead>
 				<tbody style="text-align:center">
 					<?php $no = 1; ?>
-					<?php foreach ($list_guru as $list) { ?>
-						<tr>
-							<td>
-								<?=$no;?>
-							</td>
-							<td>
-								<?=$list['nama']?>
-							</td>
-							<td>
-								<?=$list['email']?>
-							</td>
-							<td>
-								<?=$list['password']?>
-							</td>
-							<td>
-								<a href="detail.php?id_user=" class="btn btn-outline-success"><span class="iconify-inline" data-icon="clarity:note-edit-line" style="color: green;"></span></a>
-								&nbsp;
-								<a href="detail.php?id_user=" class="btn btn-danger"><span class="iconify-inline" data-icon="fluent:delete-20-regular" style="color: white;"></span></a>
-							</td>
-						</tr>
-					<?php $no++ ?>
+					<?php if (is_array($list_guru)){ ?>
+						<?php foreach ($list_guru as $list) { ?>
+							<tr>
+								<td>
+									<?=$no;?>
+								</td>
+								<td>
+									<?=$list['nama']?>
+								</td>
+								<td>
+									<?=$list['email']?>
+								</td>
+								<td>
+									<?=$list['password']?>
+								</td>
+								<td>
+									<a href="detail.php?id_user=" class="btn btn-outline-success"><span class="iconify-inline" data-icon="clarity:note-edit-line" style="color: green;"></span></a>
+									&nbsp;
+									<a href="detail.php?id_user=" class="btn btn-danger"><span class="iconify-inline" data-icon="fluent:delete-20-regular" style="color: white;"></span></a>
+								</td>
+							</tr>
+						<?php $no++ ?>
+						<?php } ?>
 					<?php } ?>
 				</tbody>
 			</table>
